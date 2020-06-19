55303ac9288efbe9b88a0e38a9fe2df5ebcc8d84
Jerry Gao <gao@blackwright.com>
2020-06-19 01:08:04 -0400

    Updates

---
+++ precache-manifest.c0e18c115116760c9788946ea50810a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26476139b3e6011e5e5d509c22d5b82a",
+    "url": "/react-router-test/index.html"
+  },
+  {
+    "revision": "8f5ae9c3f4fac7ab09ea",
+    "url": "/react-router-test/static/css/main.822411f8.chunk.css"
+  },
+  {
+    "revision": "ef1ab46836c57864c257",
+    "url": "/react-router-test/static/js/2.d16e5a4e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-router-test/static/js/2.d16e5a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5ae9c3f4fac7ab09ea",
+    "url": "/react-router-test/static/js/main.9284b1a0.chunk.js"
+  },
+  {
+    "revision": "dca393b49487dbc16bc0",
+    "url": "/react-router-test/static/js/runtime-main.28ce37fd.js"
+  }
+]);
